10af905117b5bd99c5510d16dbb7f13c1504ac40
Sri Harsh Amur <sriharshamur@gmail.com>
2022-11-28 14:15:42 +0100

    Improved footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
+
+const footerLinks = [
+  {
+    title: "Events",
+    links: [
+      { name: "View Events", link: "/events" },
+    ]
+  }
+]
+
 const Footer = () => {
   return (
     <div className=" border-t-2 border-gray-50 px-8 py-4 dark:bg-gray-900 dark:text-white">
       <div className="container mx-auto mt-4">
-        <div className="flex items-start justify-between">
+        <div className="flex flex-col items-center justify-between gap-4 text-center md:flex-row md:items-start md:gap-0 md:text-left">
           <div>
             <div className="mb-3 text-lg font-bold">Events</div>
             <div className="flex flex-col gap-1">
@@ -47,9 +57,8 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="my-6 flex items-center justify-between">
+        <div className="my-6 flex flex-col items-center justify-between md:flex-row">
           <div className=" text-2xl font-bold">AtEventz</div>
-
           <div className=" text-center">&copy; Copyright 2022 AtEventz</div>
         </div>
       </div>
